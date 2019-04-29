7e282217321fe00f04c6614f0c903e455b1ffc90
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2019-04-29 18:10:15 +0900

    refactor code

--- app/jobs/crawl_and_upload_csv_file_job.rb
+++ app/jobs/crawl_and_upload_csv_file_job.rb
@@ -6,18 +6,18 @@ class CrawlAndUploadCsvFileJob < ApplicationJob
 
   def perform(*args)
     options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument("--headless")
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-popup-blocking')
-    options.add_argument('--window-size=1440,900')
+    each_options = ["--headless", '--no-sandbox', '--disable-gpu', '--disable-popup-blocking', '--window-size=1440,900']
+    each_options.each do |option|
+      options.add_argument(option)
+    end
     driver = Selenium::WebDriver.for :chrome, options: options
     bridge = driver.send(:bridge)
     path = "/session/#{bridge.session_id}/chromium/send_command"
-    command_hash = { cmd: 'Page.setDownloadBehavior',
-                     params: {
-      behavior: 'allow',
-      downloadPath: File.absolute_path(File.expand_path('./tmp', Rails.root)) # download directory
+    command_hash = {
+      cmd: 'Page.setDownloadBehavior',
+      params: {
+        behavior: 'allow',
+        downloadPath: File.absolute_path(File.expand_path('./tmp', Rails.root)) # download dir
       }
     }
     bridge.http.call(:post, path, command_hash)
@@ -25,7 +25,6 @@ class CrawlAndUploadCsvFileJob < ApplicationJob
     driver.navigate.to url
     csv_download_link = driver.find_element(:link, 'CSVをダウンロードする')
     csv_download_link.click
-    puts 'ダウンロードされました'
   end
 end
 

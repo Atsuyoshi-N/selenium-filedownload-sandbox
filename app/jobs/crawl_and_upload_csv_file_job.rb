f71af42044d519a0478dba44391dc75c4b201525
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2019-04-30 15:41:30 +0900

    refactor code

--- app/jobs/crawl_and_upload_csv_file_job.rb
+++ app/jobs/crawl_and_upload_csv_file_job.rb
@@ -6,17 +6,14 @@ class CrawlAndUploadCsvFileJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    options = Selenium::WebDriver::Chrome::Options.new
+    chrome_options = Selenium::WebDriver::Chrome::Options.new
     download_file_name = "csv_#{Time.zone.now.strftime("%Y%m%d-%H-%M-%S")}"
-    # download_file_path = Rails.root.join('/tmp', download_file_name)
     download_file_path = File.expand_path("./tmp/#{download_file_name}", File.absolute_path(Rails.root))
-    puts download_file_name
-    puts download_file_path
-    each_options = ["--headless", '--no-sandbox', '--disable-gpu', '--disable-popup-blocking', '--window-size=1440,900']
-    each_options.each do |option|
-      options.add_argument(option)
+    options = ["--headless", '--no-sandbox', '--disable-gpu', '--disable-popup-blocking', '--window-size=1440,900']
+    options.each do |option|
+      chrome_options.add_argument(option)
     end
-    driver = Selenium::WebDriver.for :chrome, options: options
+    driver = Selenium::WebDriver.for :chrome, options: chrome_options
     bridge = driver.send(:bridge)
     path = "/session/#{bridge.session_id}/chromium/send_command"
     command_hash = {
@@ -31,23 +28,12 @@ class CrawlAndUploadCsvFileJob < ApplicationJob
     driver.navigate.to url
     csv_download_link = driver.find_element(:link, 'CSVをダウンロードする')
     download_file = csv_download_link.click
+    sleep(5)
 
-    # store = Store.last.tap do |st|
-      # st.csv_files.attach(download_file_path)
-    # end
-    # store.save
     store = Store.last
-    # puts store.csv_files
-    puts download_file_path
-    # store.csv_file.attach(io: File.open(download_file_path), filename: download_file_name, content_type: "text/csv")
-    # new_csv_file = store.csv_file.new
-    puts Dir.glob(download_file_path+"/*.csv")
+    file_location = Dir.glob(download_file_path+"/*.csv")[0]
     filename = File.open(Dir.glob(download_file_path+"/*.csv")[0][/[a-zA-Z0-9\-\_]*\.csv$/])
-    store.csv_file.attach(io: File.open(Dir.glob(download_file_path+"/*.csv")[0]), filename: filename, content_type: Mime[:csv].to_s)
-    # store.csv_file.attach(File.open(Dir.glob(download_file_path+"/*.csv")[0]))
-    # store.save
-    # File.delete(download_file_path) if File.exist?(download_file_path)
-    puts store.csv_file.attached?
+    store.csv_file.attach(io: File.open(file_location), filename: filename, content_type: Mime[:csv].to_s)
 
   end
 
